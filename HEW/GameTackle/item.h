03e4afeea37b3cfbaff1a5adae81bfa6a8145cc3
taichi-eng <56010803+taichi-eng@users.noreply.github.com>
2020-05-21 12:53:04 +0900

    muzui
    tyottomuzui

--- HEW/GameTackle/item.h
+++ HEW/GameTackle/item.h
@@ -9,16 +9,21 @@
 //*****************************************************************************
 // マクロ定義
 //*****************************************************************************
-#define	MAX_ITEM				(128)					// アイテム最大数
-#define	VALUE_ROTATE_ITEM		(D3DX_PI * 0.025f)		// 回転速度
-#define	ITEM_RADIUS				(10.0f)					// 半径
-#define ITEM_SIZE_X				(100.0f)				// アイテムの当たり判定のサイズ
-#define ITEM_SIZE_Y				(100.0f)				// アイテムの当たり判定のサイズ
-#define ITEM_SIZE_Z				(100.0f)				// アイテムの当たり判定のサイズ
+#define	MAX_ITEM				(128)						// アイテム最大数
+#define	VALUE_ROTATE_ITEM		(D3DX_PI * 0.025f)			// 回転速度
+#define	ITEM_RADIUS				(10.0f)						// 半径
+#define ITEM_EFECT_TIME			(1.0f)						// アイテムのエフェクトに掛かる時間
+#define ADD_ITEM_TIME			(1.0f/(ITEM_EFECT_TIME*60))	// エフェクトに掛かる時間の計算用(1フレームごとに加算する量)
+#define ITEM_SIZE_X				(100.0f)					// アイテムの当たり判定のサイズ
+#define ITEM_SIZE_Y				(100.0f)					// アイテムの当たり判定のサイズ
+#define ITEM_SIZE_Z				(100.0f)					// アイテムの当たり判定のサイズ
 
-#define PLAYER_SIZE_X			(100.0f)				// プレイヤーの当たり判定のサイズ
-#define PLAYER_SIZE_Y			(100.0f)				// プレイヤーの当たり判定のサイズ
-#define PLAYER_SIZE_Z			(100.0f)				// プレイヤーの当たり判定のサイズ
+#define PLAYER_SIZE_X			(100.0f)					// プレイヤーの当たり判定のサイズ
+#define PLAYER_SIZE_Y			(100.0f)					// プレイヤーの当たり判定のサイズ
+#define PLAYER_SIZE_Z			(100.0f)					// プレイヤーの当たり判定のサイズ
+
+#define ITEM_UI_POS_X			(800.0f)					// アイテムのUIのスクリーン座標X
+#define ITEM_UI_POS_Y			(100.0f)					// アイテムのUIのスクリーン座標Y
 
 //**************************************
 // 種類
@@ -35,8 +40,11 @@ enum
 typedef struct
 {
 	D3DXVECTOR3 pos;		// 現在の位置
+	D3DXVECTOR3 firstpos;	// アイテムが当たった位置
+	D3DXVECTOR3 endpos;		// アイテムの終点
 	D3DXVECTOR3 rot;		// 現在の向き
 	float fRadius;			// 半径
+	float time;				// アイテムのエフェクトに掛かる時間
 	int nIdxShadow;			// 影ID
 	int nType;				// 種類
 	bool bUse;				// 使用しているかどうか
@@ -56,11 +64,11 @@ ITEM *GetItem(void);
 
 D3DXVECTOR3 *BezierCurve(
 	D3DXVECTOR3* p_pos,
-	float t, D3DXVECTOR2*
-	p_start, D3DXVECTOR2*
-	p_second, D3DXVECTOR2*
-	p_third, D3DXVECTOR2*
-	p_end);
+	float t, 
+	D3DXVECTOR3* p_start, 
+	D3DXVECTOR3* p_second, 
+	D3DXVECTOR3* p_third, 
+	D3DXVECTOR3* p_end);
 
 D3DXVECTOR3* CalcScreenToWorld(
 	D3DXVECTOR3* p_out,
